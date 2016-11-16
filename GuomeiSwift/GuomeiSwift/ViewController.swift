c3aa5bb3e022885094a8f8be96d720abd026261b
zzxuxu <wangjianxu9515@hotmail.com>
2016-11-16 19:43:09 +0800

    项目搭建

---
+++ GuomeiSwift/GuomeiSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuomeiSwift
+//
+//  Created by 王健旭 on 2016/11/16.
+//  Copyright © 2016年 王健旭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
